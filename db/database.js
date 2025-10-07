e14d140147be418c175c1522a440d29775297e62
Dimitri Blorov <dimitri.bolorov@auxiliadorapredial.com.br>
2025-10-07 16:36:22 -0300

    Projeto Cantina - parte do index e database

---
+++ db/database.js
@@ -0,0 +1,16 @@
+let categorias = [];
+let produtos = [];
+let vendas = [];
+
+let idCategoria = 1;
+let idProduto = 1;
+let idVenda = 1;
+
+module.exports = {
+  categorias,
+  produtos,
+  vendas,
+  idCategoria,
+  idProduto,
+  idVenda
+};
