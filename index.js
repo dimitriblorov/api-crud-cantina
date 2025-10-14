39053d7911918751ab0aefa0565f480eb08e7e1e
DIMITRI FONTANA BLOROV <83770089049@senacrs.edu.br>
2025-10-14 20:00:30 -0300

    inclusão das rotas no index

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+const categoriasRoutes = require("./routes/categoriasRoutes");
+const produtoRoutes = require("./routes/produtoRoutes");
+const vendaRoutes = require("./routes/vendaRoutes");
+
+app.use("/categorias", categoriasRoutes);
+app.use("/produto", produtoRoutes);
+app.use("/venda", vendaRoutes);
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`API rodando em http://localhost:${PORT}`);
